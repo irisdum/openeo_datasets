2aa9cb5fe70f7023ea530031016c8e4d94d0b591
dumeuri <iris.dumeur@univ-tlse3.fr>
2024-02-13 14:12:27 +0100

    FIX class padding

--- src/openeo_mmdc/dataset/padding.py
+++ src/openeo_mmdc/dataset/padding.py
@@ -8,7 +8,9 @@ def apply_padding(allow_padd, max_len, t, sits, doy):
         padd_doy = (0, max_len - t)
         # padd_label = (0, 0, 0, 0, 0, self.max_len - t)
         sits = F.pad(sits, padd_tensor)
+        #print(f"before {doy.shape}")
         doy = F.pad(doy, padd_doy)
+        #print(f"after {doy.shape}")
         padd_index = torch.zeros(max_len)
         padd_index[t:] = 1
         padd_index = padd_index.bool()
